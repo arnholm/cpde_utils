e95ef41436cfbc1231c3e1615e7f021868bc286a
Carsten Arnholm <arnholm@arnholm.org>
2020-01-07 12:02:45 +0100

    enable native 64bit compiler

--- doc/wxWidgets/nmake_build_wx30_x64.cmd
+++ doc/wxWidgets/nmake_build_wx30_x64.cmd
@@ -1,6 +1,7 @@
 REM  This procedure builds wxWidgets on Windows using MS compiler and NMAKE
+REM  Procedure must be run from "Visual Studio Command Prompt"
+REM      Start -> Programs ->Visual Studio tools -> VS2013 x64 Cross Tools Command Prompt
 REM  
-REM  use any CMD command promt
 REM  change dir below as required, for example
 REM      E:\CPDE2\zdep\3rdparty\wx\x64\3.0.2\lib\build\msw
 REM
@@ -10,8 +11,8 @@ REM
 REM Below is for MSVC2013, using cross compiler
 REM call "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat" x86_amd64
 REM
-REM Below is for MSVC2019 enterprise, using native compiler
-call "%VS160COMNTOOLS%..\..\VC\Auxiliary\Build\vcvarsall.bat" x86
+REM Below is for MSVC2019, using native compiler
+call "%VS160COMNTOOLS%..\..\VC\Auxiliary\Build\vcvarsall.bat" x64
 REM
 REM ------- make sure foreign makeflags don't interfere (QNX does this)
 set MAKEFLAGS=
@@ -19,4 +20,4 @@ REM -------
 nmake -f makefile.vc TARGET_CPU=amd64 BUILD=release DEBUG_INFO=1 RUNTIME_LIBS=dynamic SHARED=0 
 nmake -f makefile.vc TARGET_CPU=amd64 BUILD=debug   DEBUG_INFO=1 RUNTIME_LIBS=dynamic SHARED=0  
 REM
-echo "==== wxWidgets build completed ====="
+echo "==== wxWidgets 3.0 build completed ====="
