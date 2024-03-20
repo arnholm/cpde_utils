8e974245ca981354914dd2036efab274826505ad
Carsten Arnholm <arnholm@arnholm.org>
2024-03-20 12:39:57 +0100

    added cpde_ISO_console template

---
+++ cpde_pwiz/UserTemplates/cpde_ISO_console/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+
+int main(int argc, char **argv)
+{
+
+   std::cout << "Hello ISO console" << std::endl;
+   return 0;
+}
+
+
